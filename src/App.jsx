d54bc9f78841456e8c5fad8af70e401f5f692828
qhahd78 <umuj0708@likelion.org>
2021-11-08 18:40:16 +0900

    #37 Feat(setting):GlobalCss, Color add close #37

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import Router from "./Router/Router";
+import GlobalCss from "./Assets/Globalcss";
+import "./Assets/fonts/pretendardvariable.css";
 
 function App() {
   return (
     <>
+      <GlobalCss />
       <Router />
     </>
   );
