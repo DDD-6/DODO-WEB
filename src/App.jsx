737db980392d5ef43eb1532ee6cf871bab27e75f
Mang-developer <chiwon618@gmail.com>
2021-11-17 02:28:27 +0900

    #12 feat: 헤드라인 global css 추가, styled-components theme 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import Router from './Router/Router';
-import GlobalCss from './Assets/Globalcss';
-import './Assets/fonts/pretendardvariable.css';
+import React from "react";
+import { ThemeProvider } from "styled-components";
+import Router from "./Router/Router";
+import GlobalCss from "./Assets/Globalcss";
+import "./Assets/fonts/pretendardvariable.css";
+import theme from "./Assets/theme";
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalCss />
       <Router />
-    </>
+    </ThemeProvider>
   );
 }
 
---
+++ src/Assets/theme.jsx
@@ -0,0 +1,18 @@
+import Colors from "./Colors/Colors";
+
+export default {
+  spacer: {
+    64: "padding-bottom: 64px;",
+    56: "padding-bottom: 56px;",
+    48: "padding-bottom: 48px;",
+    40: "padding-bottom: 40px;",
+    32: "padding-bottom: 32px;",
+    24: "padding-bottom: 24px;",
+    16: "padding-bottom: 16px;",
+    8: "padding-bottom: 8px;",
+  },
+  "spacer-w": {
+    16: "padding-left: 16px",
+  },
+  color: Colors,
+};
